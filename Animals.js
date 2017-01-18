3af6c0fe101676c567214a0e74c04768383489c2
Duunko <dursmith@ucsc.edu>
2017-01-17 17:00:03 -0800

    Basic animal data structure

---
+++ Animals.js
@@ -0,0 +1,52 @@
+
+// ------- animal class -----------------------------------------------------------------
+// Base animal class. All animal classes inherit their base attributes from the basic
+// animal class, but modify what data is fetched, the update function, and the identity
+// property.
+// Properties: 	
+// Armor - The amount of hits an animal can take before dying.
+// Speed - The speed at which the animal moves across the screen.
+// Capacity - The number of items the animal can pick up before being removed from the map.
+// Lifespan - The amount of time that the animal survives before being removed form the map.
+
+var animal = new function(){
+	this.armor = 0;
+	this.speed = 0;
+	this.capacity = 0;
+	this.lifespan = 0;
+	this.identity = 'none';
+	
+}
+
+inheritsFrom(animal, Sprite);
+
+
+	
+
+
+// ------- animal_data -------------------------------------------------------------------
+// Contains all animal data. When servers and localstorage are implemented, holds all locally
+// changed values before pushing to the server, and takes in all server data on login.
+
+animal_data = {
+	frog:{
+		armor:0,
+		speed:1,
+		lifespan: 150 //Arbitrary for testing
+	},
+	bunny:{
+		armor:0,
+		speed:1,
+		lifespan: 150 //Arbitrary for testing
+	},
+	deer:{
+		armor:0,
+		speed:1,
+		lifespan: 150 //Arbitrary for testing
+	},
+	bird:{
+		armor:0,
+		speed:1,
+		lifespan: 150 //Arbitrary for testing
+	}
+}
