1400f60ac5fb5d00a5e70c51577c89ce9b4822b1
Dan Cochran <danimaltl@gmail.com>
2017-02-20 23:48:02 -0800

    Added comments, how productive.

--- scripts/sound.js
+++ scripts/sound.js
@@ -1,11 +1,16 @@
+//Howler object holds music, may use one Howl for all sound,
+//need to figure out full structure
 var music = new Howl({
   src: ['sounds/ambisynth_livedraft.mp3'],
   buffer: true,
   loop: true
 });
 
+//This should move elsewhere maybe.
 music.play();
 
+
+//mute_music makes a nicer fade than the native mute
 var muted = false
 
 function mute_music() {
