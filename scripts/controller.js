d40ffaf82e341411d3a47a75e5a74f9e9da87ec6
Duunko <dursmith@ucsc.edu>
2017-04-06 13:14:12 -0700

    Reworks

--- scripts/controller.js
+++ scripts/controller.js
@@ -9,9 +9,7 @@ temp_storage['frog'] = 1;
 temp_storage['bunny'] =1;
 temp_storage['bird'] = 1;
 temp_storage['deer'] = 1;
-console.log(temp_storage);
 animal_types = ['bird', 'deer', 'frog', 'bunny'];
-console.log(animal_types);
 
 template_storage = temp_storage;
 for(var i = 0; i < animal_types.length; i++){
@@ -20,18 +18,38 @@ for(var i = 0; i < animal_types.length; i++){
 
 /* ANIMAL PROGRESSION MULTIPLIERS
  * 
- * Ordering: Vitality, Evasion, Strength, Athletics, Instinct, Lifespan
+ * Ordering: Speed, Evasion, Strength
+ * 
+ * CURRENT VERSION: Only three stats, but each animal type has hard coded bonuses
+ * 
+ * BONUSES: Frogs have advantage on all events that require Speed, and get double steps on MARSH
+ *          Deer have advantage on all events that require Strength, get double steps on Forest
+ *          Birds have advantage on all events that require Evasion, get double steps on Plains
+ *          Bunnies get double steps always but they have no other bonus
  * 
  */
 
 animal_data = [];
-animal_data['frog'] = [1.7, 1.2, 1.5, 1.5, 1.2, 30];
-animal_data['bunny'] = [1.2, 1.5, 1.2, 1.7, 1.5, 30];
-animal_data['deer'] = [1.5, 1.5, 1.5, 1.5, 1.5, 30];
-animal_data['bird'] = [1.2, 1.7, 1.2, 1.2, 1.7, 30];
+animal_data['frog'] = [1.6, 1.4, 1.2];
+animal_data['bunny'] = [1.4, 1.4, 1.2];
+animal_data['deer'] = [1.4, 1.2, 1.6];
+animal_data['bird'] = [1.4, 1.6, 1.2];
 
 
-console.log(animal_data['bunny']);
+
+function animalClass(type){
+	this.type = type;
+	this.level = 0;
+	
+	this.setLevel = function(lv){
+		this.level = lv;
+	}
+	
+	this.levelUp = function(){
+		this.level += 1;
+	}
+}
+
 
 /* MASTER CONTROLLER OBJECT
  *
@@ -42,127 +60,77 @@ console.log(animal_data['bunny']);
  * 
  */
 function master_controller() {
-	this.levels = [];
-	this.levels['frog'] = 1;
-	this.levels['bunny'] =1;
-	this.levels['bird'] = 1;
-	this.levels['deer'] = 1;
-	this.animal_count = [];
-	this.animal_count['frog'] = 1;
-	this.animal_count['bunny'] =0;
-	this.animal_count['bird'] = 0;
-	this.animal_count['deer'] = 1;
-	this.pending_animals = [];
-	this.pending_animals['frog'] = 0;
-	this.pending_animals['bunny'] =0;
-	this.pending_animals['bird'] = 0;
-	this.pending_animals['deer'] = 0;
-	this.pending_loss = [];
-	this.pending_loss['frog'] = 0;
-	this.pending_loss['bunny'] =0;
-	this.pending_loss['bird'] = 0;
-	this.pending_loss['deer'] = 0;
+	this.base_levels = [];
+	this.base_levels['frog'] = 1;
+	this.base_levels['bunny'] =1;
+	this.base_levels['bird'] = 1;
+	this.base_levels['deer'] = 1;
+	
+	this.animals = [];
+	
+	this.party_limit = 5;
+	
+	
 	this.timer = 0;
-	this.lifespans = new p_queue();
-	this.lifespans.push(150, 'bird');
-	this.lifespans.push(150, 'frog');
-	this.lifespans.push(150, 'bunny');
-	this.lifespans.push(150, 'deer');
+	
+	
+	//this.lifespans = new p_queue();
 	
 	this.animations = [];
 	
 	this.query = function(){
-		temp_storage = this.levels;
-		for(var i = 0; i < this.lifespans.length; i++){
-			this.lifespans.reduce(i, 15);
-		}
-		while(true){
-			if(this.lifespans.length == 0){
-				break;
-			}
-			if(this.lifespans.head() <= 0){
-				var temp = this.lifespans.pop();
-				this.pending_loss[temp]--;
-			} else {
-				break;
-			}
-		}
-		for(var i = 0; i < animal_types.length; i++){
-			if(this.pending_animals[animal_types[i]] != 0 || this.pending_loss[animal_types[i]] != 0){
-				this.animal_count[animal_types[i]] += this.pending_animals[animal_types[i]] + this.pending_loss[animal_types[i]];
-				if(this.animal_count[animal_types[i]] < 0){
-					this.animal_count[animal_types[i]] = 0;
-				}
-				for(var j = 0; j < this.pending_animals[animal_types[i]]; j++){
-					var life = this.getAnimalData(animal_types[i]);
-					this.lifespans.push(life[5], animal_types[i]);
-				}
-			}
 		
-		}
-		this.pending_animals = template_storage;
-		this.pending_loss = [];
-		this.pending_animals['frog'] = 0;
-		this.pending_animals['bunny'] =0;
-		this.pending_animals['bird'] = 0;
-		this.pending_animals['deer'] = 0;
-		this.pending_loss['frog'] = 0;
-		this.pending_loss['bunny'] =0;
-		this.pending_loss['bird'] = 0;
-		this.pending_loss['deer'] = 0;
 	}
 	
-	this.levelUp = function(animal){
-		this.levels[animal] += 1;
-		//console.log(this.levels);
+	this.baseLevelUp = function(animal){
+		this.base_levels[animal] += 1;
+	}
+	
+	this.levelUpAnimal = function(num){
+		this.animals[num].levelUp();
 	}
 	
 	this.addAnimal = function(animal){
-		this.pending_animals[animal] += 1;
-	}
-	
-	this.removeAnimal = function(animal, count){
-		this.pending_loss[animal] -= count;
-		for(var i = 0; i < count; i++){
-		    this.lifespans.popNext(animal);
-		}
-	}
-	
-	this.getAnimalData = function(animal){
-		stats = animal_data[animal].slice();
-		if(this.levels[animal] == 1){
-			return [1,1,1,1,1,1500];
+		if(this.animals.length <= this.party_size) {
+			var ani = new animalClass(animal);
+			ani.setLevel(this.base_levels[animal]);
+			this.animals.push(ani);
+			return true;
 		} else {
-			for(var i = 0; i < stats.length; i++){
-				if(i == 5){
-					multi = 1500
-				} else {
-				    var multi = 1;
-				}
-				for(var j = 0; j < this.levels[animal] - 1; j++){
-					multi = Math.ceil(multi + (this.levels[animal] * stats[i]));
-				}
-				stats[i] = multi;
-			} 
-		return stats;
+			return false;
 		}
 	}
 	
-	this.getAnimalCount = function(animal) {
-		return this.animal_count[animal];
-	}
-	
-	this.getAnimalLevel = function(animal){
-		return this.levels[animal];
-	}
-	
-	this.getAnimalTotal = function(animal){
-		var val = 0;
-		for(var i = 0; i < 4; i++){
-			val += this.getAnimalCount(animal_types[i]);
+	this.removeAnimal = function(num){
+		if(num < this.animals.length){
+			this.animals.splice(num,1);
+			return true;
+		} else {
+			return false;
 		}
-		return val;
 	}
+	
+	this.getAnimalData = function(){
+		var data = [];
+		for(var i = 0; i < this.animals.length; i++){
+			var dat = [];
+			dat.push(animals[i].type)
+			for(var j = 0; j < 3; j++){
+				var stat = 1;
+				for(var k = 0; k < animals[i].level){
+					stat = math.ciel(stat * animal_data[j]);
+				}
+				dat.push(stat);
+			}
+			data.push(dat);
+		}
+		return data;
+	}
+	
+	this.getAnimalBaseLevel = function(animal){
+		return this.base_levels[animal];
+	}
+	
 	
 	this.update = function() {
 		this.timer++;
